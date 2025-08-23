5259ada2a2c1dc0217896c89616e8013c22f7dbd
Prabhzcode <prabhhere638@gmail.com>
2025-08-23 16:54:08 +0530

    DECORATOR PROBLEM #1

---
+++ Decorators/01_solution.py
@@ -0,0 +1,16 @@
+#functions as first class objects
+#write a decorator function that measures the time taken by a fucntion to execute
+import time
+
+def timer (func):
+    def wrapper(*args,**kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time()
+        print(f" {func.__name__} ran in{end - start : .6f} time ")
+        return result
+    return wrapper 
+@timer
+def example_func(n):
+    time.sleep(n)
+example_func(2)
